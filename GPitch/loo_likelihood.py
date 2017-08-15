cd37c3e3df116d7296bbfe6acdac662448780bc3
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-08-15 16:54:58 +0100

    update logp and variational expectations

--- GPitch/loo_likelihood.py
+++ GPitch/loo_likelihood.py
@@ -33,36 +33,53 @@ class LooLik(GPflow.likelihoods.Likelihood):
         self.noise_var = GPflow.param.Param(1.0)
 
     def logp(self, F, Y):
-        f, g = F[:, 0], F[:, 1]
+        f1, g1 = F[:, 0], F[:, 1]
+        f2, g2 = F[:, 2], F[:, 3]
         y = Y[:, 0]
-        sigma_g = 1./(1 + tf.exp(-g))  # squash g to be positive
-        mean = f * sigma_g
+        sigma_g1 = 1./(1 + tf.exp(-g1))  # squash g to be positive
+        sigma_g2 = 1./(1 + tf.exp(-g2))  # squash g to be positive
+        mean = sigma_g1 * f1 + sigma_g2 * f2
         return GPflow.densities.gaussian(y, mean, self.noise_var).reshape(-1, 1)
 
-    # variational expectations function, Pablo Alvarado implementation
     def variational_expectations(self, Fmu, Fvar, Y):
-        H = 20  # get eval points and weights
-        gh_x, gh_w = GPflow.quadrature.hermgauss(H)
-        gh_x = gh_x.reshape(1, -1)
-        gh_w = gh_w.reshape(-1, 1) / np.sqrt(np.pi)
+        D = 4  # Number of input dimensions (increased from 2 to 4)
+        H = 5 # number of Gauss-Hermite evaluation points. (reduced from 10 to 3)
+        Xr, w = mvhermgauss(Fmu, tf.matrix_diag(Fvar), H, D)
+        w = tf.reshape(w, [-1, 1])
+        f1, g1 = Xr[:, 0], Xr[:, 1]
+        f2, g2 = Xr[:, 2], Xr[:, 3]
+        y = tf.tile(Y, [H**D, 1])[:, 0]
+        sigma_g1 = 1./(1 + tf.exp(-g1))  # squash g to be positive
+        sigma_g2 = 1./(1 + tf.exp(-g2))  # squash g to be positive
+        mean =  sigma_g1 * f1 + sigma_g2 * f2  #Instead of sigmoid function we use the softmax.
+        evaluations = GPflow.densities.gaussian(y, mean, self.noise_var)
+        evaluations = tf.transpose(tf.reshape(evaluations, tf.pack([tf.size(w), tf.shape(Fmu)[0]])))
+        return tf.matmul(evaluations, w)
 
-        mean_f = Fmu[:, 0]  # get mean and var of each q distribution, and reshape
-        mean_g = Fmu[:, 1]
-        var_f = Fvar[:, 0]
-        var_g = Fvar[:, 1]
-        mean_f, mean_g, var_f, var_g = [tf.reshape(e, [-1, 1]) for e in (mean_f,
-                                        mean_g, var_f, var_g)]
-        shape = tf.shape(mean_g)  # get  output shape
-        X = gh_x * tf.sqrt(2.*var_g) + mean_g  # transformed evaluation points
-        evaluations = 1. / (1. + tf.exp(-X))  # sigmoid function
-        E1 = tf.reshape(tf.matmul(evaluations, gh_w), shape)  # compute expectations
-        E2 = tf.reshape(tf.matmul(evaluations**2, gh_w), shape)
-
-        # compute log-lik expectations under variational distribution
-        var_exp = -0.5*((1./self.noise_var)*(Y**2 - 2.*Y*mean_f*E1 +
-                  (var_f + mean_f**2)*E2) + np.log(2.*np.pi) +
-                  tf.log(self.noise_var))
-        return var_exp
+    # # variational expectations function, Pablo Alvarado implementation
+    # def variational_expectations(self, Fmu, Fvar, Y):
+    #     H = 20  # get eval points and weights
+    #     gh_x, gh_w = GPflow.quadrature.hermgauss(H)
+    #     gh_x = gh_x.reshape(1, -1)
+    #     gh_w = gh_w.reshape(-1, 1) / np.sqrt(np.pi)
+    #
+    #     mean_f = Fmu[:, 0]  # get mean and var of each q distribution, and reshape
+    #     mean_g = Fmu[:, 1]
+    #     var_f = Fvar[:, 0]
+    #     var_g = Fvar[:, 1]
+    #     mean_f, mean_g, var_f, var_g = [tf.reshape(e, [-1, 1]) for e in (mean_f,
+    #                                     mean_g, var_f, var_g)]
+    #     shape = tf.shape(mean_g)  # get  output shape
+    #     X = gh_x * tf.sqrt(2.*var_g) + mean_g  # transformed evaluation points
+    #     evaluations = 1. / (1. + tf.exp(-X))  # sigmoid function
+    #     E1 = tf.reshape(tf.matmul(evaluations, gh_w), shape)  # compute expectations
+    #     E2 = tf.reshape(tf.matmul(evaluations**2, gh_w), shape)
+    #
+    #     # compute log-lik expectations under variational distribution
+    #     var_exp = -0.5*((1./self.noise_var)*(Y**2 - 2.*Y*mean_f*E1 +
+    #               (var_f + mean_f**2)*E2) + np.log(2.*np.pi) +
+    #               tf.log(self.noise_var))
+    #     return var_exp
 
 
 
