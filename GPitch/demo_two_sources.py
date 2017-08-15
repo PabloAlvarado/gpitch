b6f281b68f91a9073379f63bd9616d280b93bca0
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-08-15 15:29:23 +0100

    generate synthetic data and plot

--- GPitch/demo_two_sources.py
+++ GPitch/demo_two_sources.py
@@ -35,67 +35,73 @@ Kper1 = kper1.compute_K_symm(x)
 Kper2 = kper2.compute_K_symm(x)
 
 np.random.seed(23)
-f = np.random.multivariate_normal(np.zeros(x.shape[0]), Kper).reshape(-1, 1)
-f /= np.max(np.abs(f))
-g = np.random.multivariate_normal(np.zeros(x.shape[0]), Kenv).reshape(-1, 1)
-mean = gpi.logistic(g)*f
+f1 = np.random.multivariate_normal(np.zeros(x.shape[0]), Kper1).reshape(-1, 1)
+f2 = np.random.multivariate_normal(np.zeros(x.shape[0]), Kper2).reshape(-1, 1)
+f1 /= np.max(np.abs(f1))
+f2 /= np.max(np.abs(f2))
+g1 = np.random.multivariate_normal(np.zeros(x.shape[0]), Kenv1).reshape(-1, 1)
+g2 = np.random.multivariate_normal(np.zeros(x.shape[0]), Kenv2).reshape(-1, 1)
+mean = gpi.logistic(g1)*f1 + gpi.logistic(g2)*f2
 y = mean + np.random.randn(*mean.shape) * np.sqrt(noise_var)
 
-# split data into windows
-#ws = 500  # window size (samples)
-ws = N  # use all data at once (i.e. no windowing)
-Nw = N/ws  # number of windows
-x_l = [x[i*ws:(i+1)*ws].copy() for i in range(0, Nw)]
-y_l = [y[i*ws:(i+1)*ws].copy() for i in range(0, Nw)]
+plt.figure()
+plt.plot(x, y)
 
-jump = 20  # initialize model
-z = x_l[0][::jump].copy()
-m = modgp.ModGP(x_l[0].copy(), y_l[0].copy(), kper, kenv, z, whiten=True)
-m.likelihood.noise_var = noise_var
-m.likelihood.noise_var.fixed = True
-m.kern1.fixed = True
-m.kern2.fixed = True
-
-qm1 = [np.zeros(z.shape) for i in range(0, Nw)]  # list to save predictions
-qm2 = [np.zeros(z.shape) for i in range(0, Nw)]  # mean (qm) and variance (qv)
-qv1 = [np.zeros(z.shape) for i in range(0, Nw)]
-qv2 = [np.zeros(z.shape) for i in range(0, Nw)]
-
-maxiter = 200
-start_time = time.time()
-for i in range(Nw):
-    m.X = x_l[i].copy()
-    m.Y = y_l[i].copy()
-    m.Z = x_l[i][::jump].copy()
-    m.q_mu1._array = np.zeros(z.shape)
-    m.q_mu2._array = np.zeros(z.shape)
-    m.q_sqrt1._array = np.expand_dims(np.eye(z.size), 2)
-    m.q_sqrt2._array = np.expand_dims(np.eye(z.size), 2)
-    m.optimize(disp=1, maxiter=maxiter)
-    qm1[i], qv1[i] = m.predict_f(x_l[i])
-    qm2[i], qv2[i] = m.predict_g(x_l[i])
-print("--- %s seconds ---" % (time.time() - start_time))
-
-qm1 = np.asarray(qm1).reshape(-1, 1)
-qm2 = np.asarray(qm2).reshape(-1, 1)
-qv1 = np.asarray(qv1).reshape(-1, 1)
-qv2 = np.asarray(qv2).reshape(-1, 1)
-
-col = '#0172B2'
-plt.figure(), plt.title('Data and approximation')
-plt.plot(x, y, '.k', mew=1)
-plt.plot(x, gpi.logistic(qm2)*qm1, color=col , lw=2)
-
-plt.figure(), plt.title('Latent quasi-periodic function')
-plt.plot(x, f, '.k', mew=1)
-plt.plot(x, qm1, color=col, lw=2)
-plt.fill_between(x[:, 0], qm1[:, 0] - 2*np.sqrt(qv1[:, 0]),
-                 qm1[:, 0] + 2*np.sqrt(qv1[:, 0]),
-                 color=col, alpha=0.2)
-
-plt.figure(), plt.title('Latent envelope function')
-plt.plot(x[::5], gpi.logistic(g[::5]), '.k', mew=1)
-plt.plot(x, gpi.logistic(qm2), 'g', lw=2)
-plt.fill_between(x[:, 0], gpi.logistic(qm2[:, 0] - 2*np.sqrt(qv2[:, 0])),
-                  gpi.logistic(qm2[:, 0] + 2*np.sqrt(qv2[:, 0])),
-                  color='green', alpha=0.2)
+# # split data into windows
+# #ws = 500  # window size (samples)
+# ws = N  # use all data at once (i.e. no windowing)
+# Nw = N/ws  # number of windows
+# x_l = [x[i*ws:(i+1)*ws].copy() for i in range(0, Nw)]
+# y_l = [y[i*ws:(i+1)*ws].copy() for i in range(0, Nw)]
+#
+# jump = 20  # initialize model
+# z = x_l[0][::jump].copy()
+# m = modgp.ModGP(x_l[0].copy(), y_l[0].copy(), kper, kenv, z, whiten=True)
+# m.likelihood.noise_var = noise_var
+# m.likelihood.noise_var.fixed = True
+# m.kern1.fixed = True
+# m.kern2.fixed = True
+#
+# qm1 = [np.zeros(z.shape) for i in range(0, Nw)]  # list to save predictions
+# qm2 = [np.zeros(z.shape) for i in range(0, Nw)]  # mean (qm) and variance (qv)
+# qv1 = [np.zeros(z.shape) for i in range(0, Nw)]
+# qv2 = [np.zeros(z.shape) for i in range(0, Nw)]
+#
+# maxiter = 200
+# start_time = time.time()
+# for i in range(Nw):
+#     m.X = x_l[i].copy()
+#     m.Y = y_l[i].copy()
+#     m.Z = x_l[i][::jump].copy()
+#     m.q_mu1._array = np.zeros(z.shape)
+#     m.q_mu2._array = np.zeros(z.shape)
+#     m.q_sqrt1._array = np.expand_dims(np.eye(z.size), 2)
+#     m.q_sqrt2._array = np.expand_dims(np.eye(z.size), 2)
+#     m.optimize(disp=1, maxiter=maxiter)
+#     qm1[i], qv1[i] = m.predict_f(x_l[i])
+#     qm2[i], qv2[i] = m.predict_g(x_l[i])
+# print("--- %s seconds ---" % (time.time() - start_time))
+#
+# qm1 = np.asarray(qm1).reshape(-1, 1)
+# qm2 = np.asarray(qm2).reshape(-1, 1)
+# qv1 = np.asarray(qv1).reshape(-1, 1)
+# qv2 = np.asarray(qv2).reshape(-1, 1)
+#
+# col = '#0172B2'
+# plt.figure(), plt.title('Data and approximation')
+# plt.plot(x, y, '.k', mew=1)
+# plt.plot(x, gpi.logistic(qm2)*qm1, color=col , lw=2)
+#
+# plt.figure(), plt.title('Latent quasi-periodic function')
+# plt.plot(x, f, '.k', mew=1)
+# plt.plot(x, qm1, color=col, lw=2)
+# plt.fill_between(x[:, 0], qm1[:, 0] - 2*np.sqrt(qv1[:, 0]),
+#                  qm1[:, 0] + 2*np.sqrt(qv1[:, 0]),
+#                  color=col, alpha=0.2)
+#
+# plt.figure(), plt.title('Latent envelope function')
+# plt.plot(x[::5], gpi.logistic(g[::5]), '.k', mew=1)
+# plt.plot(x, gpi.logistic(qm2), 'g', lw=2)
+# plt.fill_between(x[:, 0], gpi.logistic(qm2[:, 0] - 2*np.sqrt(qv2[:, 0])),
+#                   gpi.logistic(qm2[:, 0] + 2*np.sqrt(qv2[:, 0])),
+#                   color='green', alpha=0.2)
