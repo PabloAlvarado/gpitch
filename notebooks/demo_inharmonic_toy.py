9af63df0cf686c69a46693e27f223dc99311b790
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-10-05 11:13:10 +0100

    introduce constrains values of variational means.

--- notebooks/demo_inharmonic_toy.py
+++ notebooks/demo_inharmonic_toy.py
@@ -20,29 +20,29 @@ gpitch.amtgp.init_settings(visible_device = '0', interactive=True) #  configure 
 
 Nc = 4 #  set kernel params
 var =  np.ones((Nc, 1))
-var = np.asarray([.5, .3, .4, .1])
+var = np.asarray([.5, .3, .2, .1])
+var *= 1./(4.*var.sum())
 beta = 0.02
 f0 = 440.00
 leng = np.ones((Nc, 1))
-leng = np.asarray([.4, .3, .2, .1])
+leng = np.asarray([1., 1., 1., 1.])
 
 kern_f = gpitch.kernels.Inharmonic(input_dim=1, lengthscales=leng, variances=var, beta=beta, f0=f0)
-kern_g = gpflow.kernels.Matern32(input_dim=1, lengthscales=0.025, variance=10.)
+kern_g = gpflow.kernels.Matern32(input_dim=1, lengthscales=0.01, variance=16.)
 
-np.random.seed(1) # generate synthetic data
-noise_var = 1e-3
+np.random.seed() # generate synthetic data
+noise_var = 1e-4
 N = 1600
 x = np.linspace(0, 0.1, N).reshape(-1,1)
 Kf = kern_f.compute_K_symm(x)
 Kg = kern_g.compute_K_symm(x)
 f = np.random.multivariate_normal(np.zeros(N), Kf, 1).reshape(-1, 1)
-f /= np.max(np.abs(f))
 g = np.random.multivariate_normal(np.zeros(N), Kg, 1).reshape(-1, 1)
 mean = logistic(g) * f
 y = mean + np.sqrt(noise_var)*np.random.randn(*mean.shape)
 
 #  set model
-np.random.seed() # initialize randomly params
+np.random.seed(29) # initialize randomly params
 Nc = 4
 init_leng_f =1.*np.random.rand(Nc, 1)
 init_var_f = 1.*np.random.rand(Nc, 1)
@@ -53,13 +53,13 @@ init_var_g = 10.*np.random.rand()
 kern1 = gpitch.kernels.Inharmonic(input_dim=1, lengthscales=init_leng_f, variances=init_var_f, beta=init_beta, f0=init_f0)
 kern2 = gpflow.kernels.Matern32(input_dim=1, lengthscales=0.05, variance=10.)
 m = gpitch.modgp.ModGP(x, y, kern_f, kern_g, x[::8].copy())
-m.q_mu2.transform = gpflow.transforms.Logistic(a=-10.0, b=10.0)
-m.q_mu1.transform = gpflow.transforms.Logistic(a=-1.0, b=1.0)
+m.q_mu1.transform = gpflow.transforms.Logistic(a=-1., b=1.)
+m.q_mu2.transform = gpflow.transforms.Logistic(a=-8.0, b=8.0)
 m.kern1.fixed = True
 m.kern2.fixed = True
 m.likelihood.noise_var = noise_var
 m.likelihood.noise_var.fixed = True
-m.optimize(disp=1, maxiter=400)
+m.optimize(disp=1, maxiter=500)
 
 mu, var = m.predict_f(x)
 plt.figure()
@@ -87,7 +87,6 @@ plt.title('g functions')
 plt.plot(x, mu, 'C0')
 plt.plot(x, g, 'g', lw=2)
 plt.legend(['prediction', 'actual functions'])
-plt.twinx()
 plt.savefig('../figures/demo_inharmonic_toy_latent_functions_g.png')
 
 plt.figure()
@@ -104,6 +103,11 @@ plt.xlim([-1, 1])
 plt.tight_layout()
 plt.savefig('../figures/demo_inharmonic_toy.png')
 #print kern.compute_Kdiag(x)
+
+
+
+
+
 
 
 
