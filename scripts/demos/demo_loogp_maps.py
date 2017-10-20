b558f7fb7f2e22826497d5f04145eeb90e68bf54
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-10-20 11:49:54 +0100

    .

--- scripts/demos/demo_loogp_maps.py
+++ scripts/demos/demo_loogp_maps.py
@@ -16,46 +16,47 @@ test_data_location = '../../../datasets/maps/test_data/'
 results_files_location = '../../../results/files/pitch_detection/'
 results_figures_location = '../../../results/figures/pitch_detection/'
 
-dec = 10
-aux_list = np.asarray([1., 2.]).reshape(-1,)
-params = {'l_act1' : 1.,
-          's_act1' : 1.,
-          'l_act2' : 1.,
-          's_act2' : 1.,
-          'l_com1' : aux_list,
-          's_com1' : aux_list,
-          'f_com1' : aux_list,
-          'l_com2' : aux_list,
-          's_com2' : aux_list,
-          'f_com2' : aux_list}
-
-# define kernels
-kern_com1 = gpitch.amtgp.Matern12CosineMix(variance=params['s_com1'],
-                                           lengthscale=params['l_com1'],
-                                           period=1./params['f_com1'],
-                                           Nh=params['s_com1'].size)
-
-kern_com2 = gpitch.amtgp.Matern12CosineMix(variance=params['s_com2'],
-                                           lengthscale=params['l_com2'],
-                                           period=1./params['f_com2'],
-                                           Nh=params['s_com2'].size)
-
-kern_act1 = gpflow.kernels.Matern32(input_dim=1, lengthscales=params['l_act1'],
-                                    variance=params['s_act1'])
-
-kern_act2 = gpflow.kernels.Matern32(input_dim=1, lengthscales=params['l_act2'],
-                                    variance=params['s_act2'])
-
-kc = [kper1, kper2]
-ka = [kenv1, kenv2]
-ws = N # winsow size in samples
-m = gpitch.loopdet.LooPDet(x=x, y=y, kern_comps=kc, kern_acts=ka, ws=ws, dec=dec, whiten=True)
-m.optimize_windowed(disp=1, maxiter=100)
-m.plot_results()
-plt.tight_layout()
-plt.savefig('../../../results/figures/demos/demo_loogp_toy_new.png')
-
-
+# dec = 10
+# aux_list = np.asarray([1., 2.]).reshape(-1,)
+# params = {'l_act1' : 1.,
+#           's_act1' : 1.,
+#           'l_act2' : 1.,
+#           's_act2' : 1.,
+#           'l_com1' : aux_list,
+#           's_com1' : aux_list,
+#           'f_com1' : aux_list,
+#           'l_com2' : aux_list,
+#           's_com2' : aux_list,
+#           'f_com2' : aux_list}
+#
+# # define kernels
+# kern_com1 = gpitch.amtgp.Matern12CosineMix(variance=params['s_com1'], lengthscale=params['l_com1'],
+#                                            period=1./params['f_com1'], Nh=params['s_com1'].size)
+#
+# kern_com2 = gpitch.amtgp.Matern12CosineMix(variance=params['s_com2'], lengthscale=params['l_com2'],
+#                                            period=1./params['f_com2'], Nh=params['s_com2'].size)
+#
+# kern_act1 = gpflow.kernels.Matern32(input_dim=1, lengthscales=params['l_act1'], variance=params['s_act1'])
+#
+# kern_act2 = gpflow.kernels.Matern32(input_dim=1, lengthscales=params['l_act2'], variance=params['s_act2'])
+#
+# kc = [kper1, kper2]
+# ka = [kenv1, kenv2]
+# ws = N # winsow size in samples
+# m = gpitch.loopdet.LooPDet(x=x, y=y, kern_comps=kc, kern_acts=ka, ws=ws, dec=dec, whiten=True)
+# m.optimize_windowed(disp=1, maxiter=100)
+# m.plot_results()
+# plt.tight_layout()
+# plt.savefig('../../../results/figures/demos/demo_loogp_toy_new.png')
+#
+# # maxiter, dec, ws = 500, 10, N  # maxiter, decimation factor, window size in samples
+# # kc, ka = [kper1, kper2], [kenv1, kenv2]
+# # model = gpitch.loopdet.LooPDet(x=x, y=y, kern_comps=kc, kern_acts=ka, ws=ws, dec=dec, whiten=True)
+# # model.m.likelihood.noise_var
+# # model.optimize_windowed(disp=1, maxiter=maxiter)
+# # model.plot_results()
+# # plt.tight_layout()
+# # plt.savefig('../../../results/figures/demos/demo_loogp_maps.png')
 
 
 
