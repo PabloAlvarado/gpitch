0b077607335ed21f23aba7ff0ef430a9a4a89eb7
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2017-11-08 17:21:50 +0000

    rename variables in modGP class

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,6 @@
 # Release 0.2.0
- - This version uses stochastic variational inference for maximazing the ELBO
+ - This version uses stochastic variational inference for maximazing the ELBO in
+ the modulated GP model.
 
 # Release 0.1.0
 The initial release of gpitch.
--- gpitch/modgp.py
+++ gpitch/modgp.py
@@ -8,45 +8,47 @@ float_type = settings.dtypes.float_type
 
 
 class ModGP(gpflow.model.Model):
-    def __init__(self, X, Y, kern1, kern2, Z, whiten=False, minibatch_size=None):
+    def __init__(self, x, y, kern_com, kern_act, z, whiten=True,
+                 minibatch_size=None):
         gpflow.model.Model.__init__(self)
-        if minibatch_size is None:
-            minibatch_size = X.shape[0]
-        self.num_data = X.shape[0]
-        #self.X = X
-        #self.Y = Y
-        self.X = MinibatchData(X, minibatch_size, np.random.RandomState(0))
-        self.Y = MinibatchData(Y, minibatch_size, np.random.RandomState(0))
-        self.Z = gpflow.param.DataHolder(Z, on_shape_change='pass')
 
-        self.kern1, self.kern2 = kern1, kern2
+        if minibatch_size is None:
+            minibatch_size = x.shape[0]
+        self.num_data = x.shape[0]
+
+        self.x = MinibatchData(x, minibatch_size, np.random.RandomState(0))
+        self.y = MinibatchData(y, minibatch_size, np.random.RandomState(0))
+        self.z = gpflow.param.DataHolder(z, on_shape_change='pass')
+
+        self.kern_com = kern_com
+        self.kern_act = kern_act
         self.likelihood = ModLik()
-        self.num_inducing = Z.shape[0]
+        self.num_inducing = z.shape[0]
         self.whiten = whiten
 
         # initialize variational parameters
-        self.q_mu1 = gpflow.param.Param(np.zeros((self.Z.shape[0], 1)))
-        self.q_mu2 = gpflow.param.Param(np.zeros((self.Z.shape[0], 1)))
+        self.q_mu_com = gpflow.param.Param(np.zeros((self.z.shape[0], 1)))
+        self.q_mu_act = gpflow.param.Param(np.zeros((self.z.shape[0], 1)))
         q_sqrt = np.array([np.eye(self.num_inducing)
                            for _ in range(1)]).swapaxes(0, 2)
-        self.q_sqrt1 = gpflow.param.Param(q_sqrt.copy())
-        self.q_sqrt2 = gpflow.param.Param(q_sqrt.copy())
+        self.q_sqrt_com = gpflow.param.Param(q_sqrt.copy())
+        self.q_sqrt_act = gpflow.param.Param(q_sqrt.copy())
 
     def build_prior_KL(self):
         if self.whiten:
-            KL1 = gpflow.kullback_leiblers.gauss_kl_white(self.q_mu1,
-                                                          self.q_sqrt1)
-            KL2 = gpflow.kullback_leiblers.gauss_kl_white(self.q_mu2,
-                                                          self.q_sqrt2)
+            KL1 = gpflow.kullback_leiblers.gauss_kl_white(self.q_mu_com,
+                                                          self.q_sqrt_com)
+            KL2 = gpflow.kullback_leiblers.gauss_kl_white(self.q_mu_act,
+                                                          self.q_sqrt_act)
         else:
-            K1 = self.kern1.K(self.Z) + \
+            K1 = self.kern_com.K(self.z) + \
                  tf.eye(self.num_inducing, dtype=float_type) * settings.numerics.jitter_level
-            K2 = self.kern2.K(self.Z) + \
+            K2 = self.kern_act.K(self.z) + \
                  tf.eye(self.num_inducing, dtype=float_type) * settings.numerics.jitter_level
-            KL1 = gpflow.kullback_leiblers.gauss_kl(self.q_mu1,
-                                                    self.q_sqrt1, K1)
-            KL2 = gpflow.kullback_leiblers.gauss_kl(self.q_mu2,
-                                                    self.q_sqrt2, K2)
+            KL1 = gpflow.kullback_leiblers.gauss_kl(self.q_mu_com,
+                                                    self.q_sqrt_com, K1)
+            KL2 = gpflow.kullback_leiblers.gauss_kl(self.q_mu_act,
+                                                    self.q_sqrt_act, K2)
         return KL1 + KL2
 
     def build_likelihood(self):
@@ -54,44 +56,48 @@ class ModGP(gpflow.model.Model):
         KL = self.build_prior_KL()
 
         # Get conditionals
-        fmean1, fvar1 = gpflow.conditionals.conditional(self.X, self.Z,
-                                                        self.kern1, self.q_mu1,
-                                                        q_sqrt=self.q_sqrt1,
+        fmean1, fvar1 = gpflow.conditionals.conditional(self.x, self.z,
+                                                        self.kern_com, self.q_mu_com,
+                                                        q_sqrt=self.q_sqrt_com,
                                                         full_cov=False,
                                                         whiten=self.whiten)
-        fmean2, fvar2 = gpflow.conditionals.conditional(self.X, self.Z,
-                                                        self.kern2, self.q_mu2,
-                                                        q_sqrt=self.q_sqrt2,
+        fmean2, fvar2 = gpflow.conditionals.conditional(self.x, self.z,
+                                                        self.kern_act, self.q_mu_act,
+                                                        q_sqrt=self.q_sqrt_act,
                                                         full_cov=False,
                                                         whiten=self.whiten)
         fmean = tf.concat([fmean1, fmean2], 1)
         fvar = tf.concat([fvar1, fvar2], 1)
 
         # Get variational expectations.
-        var_exp = self.likelihood.variational_expectations(fmean, fvar, self.Y)
+        var_exp = self.likelihood.variational_expectations(fmean, fvar, self.y)
 
         # re-scale for minibatch size
         scale = tf.cast(self.num_data, settings.dtypes.float_type) / \
-            tf.cast(tf.shape(self.X)[0], settings.dtypes.float_type)
+            tf.cast(tf.shape(self.x)[0], settings.dtypes.float_type)
 
         return tf.reduce_sum(var_exp) * scale - KL
 
     @gpflow.param.AutoFlow((tf.float64, [None, None]))
-    def predict_f(self, Xnew):
-        return gpflow.conditionals.conditional(Xnew, self.Z, self.kern1,
-                                               self.q_mu1, q_sqrt=self.q_sqrt1,
+    def predict_com(self, xnew):
+        return gpflow.conditionals.conditional(xnew, self.z, self.kern_com,
+                                               self.q_mu_com, q_sqrt=self.q_sqrt_com,
                                                full_cov=False,
                                                whiten=self.whiten)
 
     @gpflow.param.AutoFlow((tf.float64, [None, None]))
-    def predict_g(self, Xnew):
-        return gpflow.conditionals.conditional(Xnew, self.Z, self.kern2,
-                                               self.q_mu2, q_sqrt=self.q_sqrt2,
+    def predict_act(self, xnew):
+        return gpflow.conditionals.conditional(xnew, self.z, self.kern_act,
+                                               self.q_mu_act, q_sqrt=self.q_sqrt_act,
                                                full_cov=False,
                                                whiten=self.whiten)
 
 
-
+"""
+________________________________________________________________________________
+This methods correspond to optimization using SVI
+________________________________________________________________________________
+"""
 
 
 
