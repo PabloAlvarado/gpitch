afeb6a83e47de48fd585a3bd9ece3bc7d980a820
PabloAlvarado <p.a.alvaradoduran@qmul.ac.uk>
2018-03-14 12:22:30 +0000

    Implementation of the Matern32 spectral mixture.

--- kernels.py
+++ kernels.py
@@ -192,6 +192,53 @@ class MaternSpecMix(gpflow.kernels.Kern):
             var += tf.fill(tf.stack([tf.shape(X)[0]]), tf.squeeze(getattr(self, 'variance_' + str(i))))
         return var
 
+class Matern32SpecMix(gpflow.kernels.Kern):
+    """
+    Matern spectral mixture kernel with single lengthscale.
+    """
+    def __init__(self, input_dim, numc, lengthscales=None, variances=None, frequencies=None):
+        gpflow.kernels.Kern.__init__(self, input_dim, active_dims=None)
+        self.ARD = False
+        self.numc = numc
+
+        if lengthscales == None:
+            lengthscales = 0.05
+            variances = 0.125*np.ones((numc, 1))
+            frequencies = 1.*np.arange(1, numc+1)
+
+        self.lengthscales = Param(lengthscales, transforms.Logistic(0., 20.0) )
+        for i in range(self.numc): # generate a param object for each  var, and freq, they must be (numc,) arrays.
+            setattr(self, 'variance_' + str(i+1), Param(variances[i], transforms.Logistic(0., 0.25) ) )
+            setattr(self, 'frequency_' + str(i+1), Param(frequencies[i], transforms.positive ) )
+
+
+    def K(self, X, X2=None, presliced=False):
+        if not presliced:
+            X, X2 = self._slice(X, X2)
+        if X2 is None:
+            X2 = X
+
+        # Introduce dummy dimension so we can use broadcasting
+        f = tf.expand_dims(X, 1)  # now N x 1 x D
+        f2 = tf.expand_dims(X2, 0)  # now 1 x M x D
+        r = tf.sqrt(tf.square(f - f2 +  1e-12))
+
+        r1 = np.sqrt(3.)*tf.reduce_sum(r / self.lengthscales, 2)
+        r2 = tf.reduce_sum(2.*np.pi * self.frequency_1 * r , 2)
+        k = self.variance_1 * (1. + r1) * tf.exp(-r1) * tf.cos(r2)
+
+        for i in range(2, self.numc + 1):
+            r1 = tf.reduce_sum(r / self.lengthscales, 2)
+            r2 = tf.reduce_sum(2.*np.pi * getattr(self, 'frequency_' + str(i)) * r , 2)
+            k += getattr(self, 'variance_' + str(i)) * (1. + r1) * tf.exp(-r1) * tf.cos(r2)
+        return k
+
+
+    def Kdiag(self, X):
+        var = tf.fill(tf.stack([tf.shape(X)[0]]), tf.squeeze(self.variance_1))
+        for i in range(2, self.numc + 1):
+            var += tf.fill(tf.stack([tf.shape(X)[0]]), tf.squeeze(getattr(self, 'variance_' + str(i))))
+        return var
 
 
 class MaternSpecMixSLS(gpflow.kernels.Kern):
